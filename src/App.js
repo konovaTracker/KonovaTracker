75a6495f411c9babb96942d068cd0b26889546d1
konovaTracker <konovaholdings@gmail.com>
2026-04-07 17:45:37 +0200

    Initialize React app with basic App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Konova Tracker Frontend</h1>
+      <p>Your React app is running successfully!</p>
+      <p>Connect to your backend API to start tracking users and analytics.</p>
+    </div>
+  );
+}
+
+export default App;
